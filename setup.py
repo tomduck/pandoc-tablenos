60dbe363ce7c814fcc853d93c5d5f9fc76ce32ec
Thomas Duck <tomduck@tomduck.ca>
2016-08-21 18:15:52 -0300

    Updated pandoc-xnos requirement.

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     download_url='https://github.com/tomduck/pandoc-tablenos/tarball/' + \
                  VERSION,
 
-    install_requires=['pandoc-xnos>=0.4',
+    install_requires=['pandoc-xnos>=0.4.1',
                       'pandocfilters',
                       'pandoc-attributes'],
 
