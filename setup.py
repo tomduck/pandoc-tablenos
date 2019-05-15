e8651d8fc3531e8d133003c2152f063b9313fad1
Thomas Duck <tomduck@tomduck.ca>
2019-05-15 10:56:02 -0300

    Preparing new release.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ LONG_DESCRIPTION = """\
 A pandoc filter for numbering tables and table references.
 """
 
-VERSION = '1.3.1'
+VERSION = '1.3.2'
 
 
 #-----------------------------------------------------------------------------
