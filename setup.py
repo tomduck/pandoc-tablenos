5494c313a69922385e0ba0a9b91e547850e87e4a
Thomas Duck <tomduck@tomduck.ca>
2019-09-23 10:52:01 -0300

    Fixed and updated dependency.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     download_url='https://github.com/tomduck/pandoc-tablenos/tarball/' + \
                  __version__,
 
-    install_requires=['pandoc-xnos~=2.0.0'],
+    install_requires=['pandoc-xnos~=2.1'],
 
     py_modules=['pandoc_tablenos'],
     entry_points={'console_scripts':['pandoc-tablenos = pandoc_tablenos:main']},
