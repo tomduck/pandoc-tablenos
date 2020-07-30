3ab7e2ab0d03a635d2a5753d48cbe1c786fba78d
Thomas J. Duck <tomduck@tomduck.ca>
2020-07-30 13:14:53 -0300

    Bumped pandoc-xnos requirement to 2.4.1.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     download_url='https://github.com/tomduck/pandoc-tablenos/tarball/' + \
                  __version__,
 
-    install_requires=['pandoc-xnos >= 2.4.0, < 3.0'],
+    install_requires=['pandoc-xnos >= 2.4.1, < 3.0'],
 
     py_modules=['pandoc_tablenos'],
     entry_points={'console_scripts':['pandoc-tablenos = pandoc_tablenos:main']},
